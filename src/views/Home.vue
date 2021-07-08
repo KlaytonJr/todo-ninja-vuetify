f00a5b142547dc3f7909a0e9cbb3237e63af6ce8
Klayton Junior <klaytonjunior@protonmail.com>
2021-07-08 10:59:53 -0300

    Informations: 	- Buttons & Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat dolores rem deserunt obcaecati delectus hic ullam ratione blanditiis! Hic ipsam quo ipsa illo assumenda iusto a voluptatum minus culpa.</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat dolores rem deserunt obcaecati delectus hic ullam ratione blanditiis! Hic ipsam quo ipsa illo assumenda iusto a voluptatum minus culpa.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="headline">this is a headline</p>
-    <p class="subheading">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    
+    <v-btn class="pink white--text">Click me</v-btn>
+
+    <v-btn dark color="pink">Click me</v-btn>
+
+    <v-btn depressed color="pink">Click me</v-btn> 
+    <!-- Remove the box-shadow -->
+
+    <v-btn flat>Click me</v-btn>
+    <!-- Background transparent -->
+
+    <!-- ICONS -->
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon large class="pl-4">mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
