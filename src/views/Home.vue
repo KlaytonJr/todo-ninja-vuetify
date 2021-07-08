8fd2d72a915abb7d0ac7c151e83fc7d426234cf1
Klayton Junior <klaytonjunior@protonmail.com>
2021-07-07 22:04:21 -0300

    Informations: 	- Text & Colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat dolores rem deserunt obcaecati delectus hic ullam ratione blanditiis! Hic ipsam quo ipsa illo assumenda iusto a voluptatum minus culpa.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat dolores rem deserunt obcaecati delectus hic ullam ratione blanditiis! Hic ipsam quo ipsa illo assumenda iusto a voluptatum minus culpa.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">this is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
