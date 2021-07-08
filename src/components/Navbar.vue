2fc93a835c7c7790b85a589c1ac6ac802b9f39bd
Klayton Junior <klaytonjunior@protonmail.com>
2021-07-08 13:18:32 -0300

    Informations: 	- Lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,16 @@
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
-            <p class="error">test</p>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -27,7 +36,12 @@ export default {
 
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/'},
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects'},
+                { icon: 'mdi-account', text: 'Team', route: '/team'},
+            ]
         }
     }
 }
