ba60feccdfeafc8a82fc980c7c03ed3859c9efde
Klayton Junior <klaytonjunior@protonmail.com>
2021-07-09 20:27:42 -0300

    Informations: 	- Dummy Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,25 +4,27 @@
     
     <!-- v-container stay centralized -->
     <v-container fluid class="my-5">
-      
-      <v-card flat class="px-3">
-        <v-row row wrap class="pa-3">
-          <v-col xs="12" md="6">
+      <v-card flat tile class="pa-3" v-for="project in projects" :key="project.title">
+        <v-row row wrap :class="`pa-3 project ${project.status}`">
+          <v-col cols="12" md="6">
             <div class="caption grey--text">Project title</div>
-            <div>Create a new website</div>
+            <div>{{project.title}}</div>
           </v-col>
-          <v-col xs="6" sm="4" md="2">
+          <v-col xs="2">
             <div class="caption grey--text">Person</div>
-            <div>The Net Ninja</div>
+            <div>{{project.person}}</div>
           </v-col>
-          <v-col xs="6" sm="4" md="2">
+          <v-col xs="2">
             <div class="caption grey--text">Due by</div>
-            <div>1st Jan 2019</div>
+            <div>{{project.due}}</div>
           </v-col>
-          <v-col xs="2" sm="4" md="2">
+          <v-col xs="2">
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{project.status}}</div>
           </v-col>
+        </v-row>
+        <v-row cols="12">
+          <v-divider></v-divider>
         </v-row>
       </v-card>
 
@@ -33,5 +35,30 @@
 <script>
   export default {
     name: 'Dashboard',
+
+    data() {
+      return {
+        projects: [
+          { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+      }
+    }
   }
 </script>
+
+<style>
+
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+
+</style>
