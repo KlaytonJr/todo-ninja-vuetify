eb2a11ccdc642c72ca108537e600f3cee9ff925d
Klayton Junior <klaytonjunior@protonmail.com>
2021-07-09 22:12:03 -0300

    Informations: 	- Avatars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,6 +16,16 @@
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-row >
+                <v-col class="mt-5 text-center" >
+                    <v-avatar size="100">
+                    <img src="/avatar-1.png" alt="" />
+                    </v-avatar>
+                    <p class="white--text text-subtitle-1 mt-1">
+                    The Net Ninja
+                    </p>
+                </v-col >
+            </v-row >
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -7,7 +7,13 @@
         <v-col cols="12" sm="6" md="4" lg="3" v-for="person in team" :key="person.name">
           <v-card flat class="text-center ma-3">
             <v-responsive class="pt-4">
-              image goes here
+              <v-row justify="center">
+                <v-col cols="6">
+                  <v-avatar size="100" class="grey lighten-2">
+                    <img :src="person.avatar" :alt="person.name"/>
+                  </v-avatar>
+                </v-col>
+              </v-row>
             </v-responsive>
             <v-card-text>
               <div class="text-subtitle-1">{{ person.name }}</div>
@@ -33,11 +39,11 @@
     data() {
       return {
         team: [
-          { name: 'The Net Ninja', role: 'Web developer' },
-          { name: 'Ryu', role: 'Graphic designer' },
-          { name: 'Chun Li', role: 'Web developer' },
-          { name: 'Gouken', role: 'Social media maverick' },
-          { name: 'Yoshi', role: 'Sales guru' }
+          { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+          { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+          { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+          { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+          { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png' }
         ]
       }
     }
